a486339aa82e8a2e08451666a091bf3d019b0639
chapulin <chapulin@chapulin-desktop>
2020-04-27 21:53:18 -0300

    Origin

---
+++ restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/HelloWorldBean.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.restful.webservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+	
+
+}
