bf5edf26c5bd92a479976643beb92f0c2fcb45c4
EIRAS DIEGO LUIS <deiras1@abc.gob.ar>
2020-05-12 22:19:43 -0300

    12/5/2020

--- restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/todo/TodoResource.java
+++ restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/todo/TodoResource.java
@@ -3,12 +3,13 @@ package com.in28minutes.rest.webservices.restful.webservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 	
