f8ab317053af9fa8b4f3328b50b0dd42894e4af9
EIRAS DIEGO LUIS <deiras1@abc.gob.ar>
2020-05-29 16:33:19 -0300

    29/5/2020

--- restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/todo/TodoResource.java
+++ restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/todo/TodoResource.java
@@ -3,7 +3,9 @@ package com.in28minutes.rest.webservices.restful.webservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +23,17 @@ public class TodoResource {
 		return todoService.findAll();
 		
 	}
-}
+	
+	//DELETE /delete/{username}/todos/{id}
+	@DeleteMapping("/delete/{username}/todos/{id}")
+	public ResponseEntity <Void> deleteTodo(
+			@PathVariable String userName, @PathVariable long id){
+		Todo todo = todoService.deleteById(id);
+		if(todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+}	
