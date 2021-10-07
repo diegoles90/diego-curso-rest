b5419f61ef1a59bbecb5875708b6dc044dba6f4a
diegoles90 <diegoles90@hotmail.com>
2021-10-06 22:00:46 -0300

    aaaaaaaaa

---
+++ restful.web-services/src/main/java/com/in28minutes/rest/webservices/restful/webservices/basic/auth/AuthenticationBean.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.restful.webservices.basic.auth;
+
+public class AuthenticationBean {
+
+	private String message;
+
+
+
+	public AuthenticationBean(String message) {
+		this.message = message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+	
+
+}
